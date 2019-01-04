822f4aeb4bab30cd6e64e3b3419db8cddbb5ffca
Samir Boulil <samir.boulil@akeneo.com>
2019-01-04 18:37:55 +0100

    Working library with books and copies

--- test/LibraryTest.hs
+++ test/LibraryTest.hs
@@ -1,20 +1,34 @@
 import Test.Hspec
 
+refactoring = Book "ref-1234" "Refactoring" "Martin Fowler"
+xpExplained = Book "ref-456" "XP Explained" "Kent Beck"
+
 main :: IO ()
 main = hspec $ do
 
-  describe "List the books of the Akeneo Library"$ do
+  describe "Manage the Akeneo Library"$ do
     it "Does not have any books" $
-      listBooks (Library []) `shouldBe` []
+      books (Library []) `shouldBe` []
+
+    it "references books" $
+      books (Library [(refactoring, 1), (xpExplained, 1)]) `shouldBe` [(refactoring, 1), (xpExplained, 1)]
+
+    it "references a new book to an existing library" $
+      referenceBook library refactoring `shouldBe` (Library [(refactoring, 1), (xpExplained, 1)])
+      where library = Library [(xpExplained, 1)]
+
+    {-TODO-}
+    {-it "can have multiple copies of the same book" $-}
+      {-referenceBook library refactoring `shouldBe` (Library [refactoring, xpExplained])-}
+      {-where library = Library [xpExplained]-}
 
 
-data ISBN = ISBN String deriving (Show, Eq)
-data Title = Title String deriving (Show, Eq)
-data Author = Author String deriving (Show, Eq)
-data Book = Book ISBN Title Author deriving (Show, Eq)
-data Library = Library [Book] deriving (Show, Eq)
+type ISBN = String
+type Title = String
+type Author = String
+data Book = Book { isbn :: ISBN, title :: Title, author:: Author } deriving (Show, Eq)
+type Copies = Integer
+data Library = Library { books :: [(Book, Copies)] } deriving (Show, Eq)
 
-
-listBooks :: Library -> [Book]
-listBooks _ = []
-
+referenceBook :: Library -> Book -> Library
+referenceBook library newBook = library { books = (newBook, 1):(books library) }
